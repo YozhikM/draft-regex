d365715e08fe03b0c4a836f0fb340dd2d45608b3
YozhikM <botev.st@gmail.com>
2018-01-30 14:30:10 +0600

    test: add tests for clearPastedStyle

---
+++ __tests__/clearPastedStyle-test.js
@@ -0,0 +1,127 @@
+/* @flow */
+
+import { EditorState, ContentState, convertFromHTML } from "draft-js";
+import { pastedHTMLWOOptions, pastedHTMLBlockTypes } from "../mocks/pastedHTML";
+import clearPastedStyle from "../src/clearPastedStyle";
+
+describe("clearPastedStyle", () => {
+  it("clearPastedStyle WO options", () => {
+    const blocks = convertFromHTML(pastedHTMLWOOptions);
+    const contentState = ContentState.createFromBlockArray(blocks);
+    const editorState = EditorState.createWithContent(contentState);
+
+    const types = [];
+    editorState
+      .getCurrentContent()
+      .getBlockMap()
+      .forEach(block => {
+        types.push(block.getType());
+      });
+
+    expect(types).toEqual(["unordered-list-item", "unordered-list-item"]);
+
+    const clearEditorState = clearPastedStyle(editorState);
+
+    const newTypes = [];
+    clearEditorState
+      .getCurrentContent()
+      .getBlockMap()
+      .forEach(block => {
+        newTypes.push(block.getType());
+      });
+
+    expect(newTypes).toEqual(["unstyled", "unstyled"]);
+  });
+
+  it("clearPastedStyle with blockTypes", () => {
+    const blocks = convertFromHTML(pastedHTMLBlockTypes);
+    const contentState = ContentState.createFromBlockArray(blocks);
+    const editorState = EditorState.createWithContent(contentState);
+
+    const types = [];
+    editorState
+      .getCurrentContent()
+      .getBlockMap()
+      .forEach(block => {
+        types.push(block.getType());
+      });
+
+    expect(types).toEqual(["header-three", "code-block", "unstyled"]);
+
+    const clearEditorState = clearPastedStyle(editorState, {
+      blockTypes: ["header-three"]
+    });
+
+    const newTypes = [];
+    clearEditorState
+      .getCurrentContent()
+      .getBlockMap()
+      .forEach(block => {
+        newTypes.push(block.getType());
+      });
+
+    expect(newTypes).toEqual(["header-three", "unstyled", "unstyled"]);
+  });
+
+  it("clearPastedStyle with replaceHeader", () => {
+    const blocks = convertFromHTML(pastedHTMLBlockTypes);
+    const contentState = ContentState.createFromBlockArray(blocks);
+    const editorState = EditorState.createWithContent(contentState);
+
+    const types = [];
+    editorState
+      .getCurrentContent()
+      .getBlockMap()
+      .forEach(block => {
+        types.push(block.getType());
+      });
+
+    expect(types).toEqual(["header-three", "code-block", "unstyled"]);
+
+    const clearEditorState = clearPastedStyle(editorState, {
+      replaceHeader: true,
+      shiftHeader: "header-one"
+    });
+
+    const newTypes = [];
+    clearEditorState
+      .getCurrentContent()
+      .getBlockMap()
+      .forEach(block => {
+        newTypes.push(block.getType());
+      });
+
+    expect(newTypes).toEqual(["header-one", "unstyled", "unstyled"]);
+  });
+
+  it("clearPastedStyle with replaceList", () => {
+    const blocks = convertFromHTML(pastedHTMLWOOptions);
+    const contentState = ContentState.createFromBlockArray(blocks);
+    const editorState = EditorState.createWithContent(contentState);
+
+    const types = [];
+    editorState
+      .getCurrentContent()
+      .getBlockMap()
+      .forEach(block => {
+        types.push(block.getType());
+      });
+
+    expect(types).toEqual(["unordered-list-item", "unordered-list-item"]);
+
+    const clearEditorState = clearPastedStyle(editorState, {
+      replaceList: true,
+      shiftList: "ordered-list-item"
+    });
+
+    const newTypes = [];
+    clearEditorState
+      .getCurrentContent()
+      .getBlockMap()
+      .forEach(block => {
+        newTypes.push(block.getType());
+      });
+
+    expect(newTypes).toEqual(["ordered-list-item", "ordered-list-item"]);
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/* @flow */
+
+module.exports = {
+  setupFiles: ['./jest.setup.js'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+};
